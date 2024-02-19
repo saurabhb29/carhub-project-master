9a5c97bdec86490d9cae80d4f5bef5a46bfe9291
Saurabh Bisht <1999saurabhbisht@gmail.com>
2024-02-19 17:37:43 +0530

    v1.1

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,3 +1,37 @@
 import { LightningElement } from 'lwc';
 
-export default class CarCard extends LightningElement {}
+// Car Schema
+import NAME_FIELD  from '@salesforce/schema/Car__c.Name';
+import PICTURE_URL_FILED from '@salesforce/schema/Car__c.Picture_URL__c';
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c';
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c';
+import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c';
+import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel_Type__c';
+import SEATS_FIELD from '@salesforce/schema/Car__c.Seats__c';
+import CONTROL_FIELD from '@salesforce/schema/Car__c.Control__c';
+
+import {getFieldValue} from 'lightning/uiRecordApi';
+// import PRODUCT_IMAGES from '@salesforce/resourceUrl/carhubimages';
+export default class CarCard extends LightningElement {
+    // exposing  fields to make available  in the template
+    categoryField = CATEGORY_FIELD;
+    makeField = MAKE_FIELD;
+    msrpField = MSRP_FIELD;
+    fuelField = FUEL_FIELD;
+    seatField = SEATS_FIELD;
+    controlField = CONTROL_FIELD;
+
+    // Id of Car__C object to display
+    recordId = 'a0I5j000007MyoTEAS';
+    carName;
+    carPictureUrl;
+
+    handleRecordLoaded(event){
+        const {records} = event.detail;
+        const recordData = records[this.recordId];
+        this.carName =  getFieldValue(recordData,NAME_FIELD );
+        this.carPictureUrl = getFieldValue(recordData,PICTURE_URL_FILED );
+    }
+
+
+}
